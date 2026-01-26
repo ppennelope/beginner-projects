e5a38c8b6447bffdf5a09d5c836318aafbd14888
Rotinda Mertay ÖNCEL <mertoncell01@gmail.com>
2026-01-26 20:49:33 +0300

    Add files via upload

---
+++ asal_sayilar/AsalSayilar.java
@@ -0,0 +1,25 @@
+package sa;
+
+public class AsalSayilar {
+
+    public static void main(String[] args) {
+
+        System.out.println("1 ile 100 arasindaki asal sayilar:");
+
+        for (int sayi = 2; sayi <= 100; sayi++) {
+
+            boolean asalMi = true;
+
+            for (int i = 2; i <= sayi / 2; i++) {
+                if (sayi % i == 0) {
+                    asalMi = false;
+                    break;
+                }
+            }
+
+            if (asalMi) {
+                System.out.print(sayi + " ");
+            }
+        }
+    }
+}
